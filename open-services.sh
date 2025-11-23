b54b8a058f1c16736a12fb336a20ac226ca1d0cf
Josward <joswrod@outlook.com>
2025-11-23 09:16:07 -0600

    se cambio el open-services.sh

--- open-services.sh
+++ open-services.sh
@@ -9,8 +9,8 @@ minikube service grafana-service -n application &
 echo "📈 Abriendo Prometheus..."
 minikube service prometheus-service -n monitoring &
 
-echo "📈 ejecutando aplicacion de mareas" 
-minikube service clima-app-service --url -n application
+echo "📈 ejecutando aplicacion de mareas..." 
+minikube service clima-app-service -n application &
 
 
 echo "✅ Todos los servicios se están abriendo en segundo plano."
